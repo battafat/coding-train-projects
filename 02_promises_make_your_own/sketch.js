0b3cce29c574c9bd51e689c3c012a5455f1b615f
timothy battafarano <tim@deepsprings.edu>
2025-06-25 15:57:31 -0500

    added index.html file from https://github.com/CodingTrain/website-archive/blob/main/Tutorials/P5JS/16_promises/13_16_async_await/index.html

--- 02_promises_make_your_own/sketch.js
+++ 02_promises_make_your_own/sketch.js
@@ -1,8 +1,13 @@
 function setup(){
     noCanvas();
-    delay(1000)
+    delayES8(1000)
         .then(() => createP('hello'))
         .catch((err) => console.error(err));
+}
+
+async function delayES8(time){
+    await delay(time);
+    return;
 }
 
 function delay(time){
@@ -10,8 +15,10 @@ function delay(time){
     return new Promise((resolve, reject) => {
         if(isNaN(time)){
             reject(new Error('delay requires a valid number'));
+        }else{
+            setTimeout(resolve, time);
         }
-        setTimeout(resolve, time);
+        
     });
 }
 
